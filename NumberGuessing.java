5de2cc966872efd51a76b930e03c9d27b373d093
Disha Sandip Surana <155512095+D-31Surana@users.noreply.github.com>
2024-02-05 14:36:27 +0530

    Update NumberGuessing.java

--- NumberGuessing.java
+++ NumberGuessing.java
@@ -1,51 +1,43 @@
-
-import javax.swing.*;
-public class class1 {
-
+package disha;
+import java.util.Random;
+import java.util.Scanner;
+public class NumberGuessingGame {
 	public static void main(String[] args) {
-	        playGame();
-	    }
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
-	    public static void playGame() {
-	        int compNumber = (int) (Math.random() * 100 + 1);
-	        int userAns = 0;
-	        int count = 1;
+        int lowerBound = 1;
+        int upperBound = 100;
+        int randomNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
 
-	        while (userAns != compNumber) {
-	            String response = JOptionPane.showInputDialog(null,
-	                    "Enter a guess between 1 and 100", "Number Guessing Game", JOptionPane.PLAIN_MESSAGE);
-	            try {
-	                userAns = Integer.parseInt(response);
+        int maxAttempts = 5;
+        int score = 0;
+        boolean gameWon = false;
 
-	                if (userAns <= 0 || userAns > 100) {
-	                    JOptionPane.showMessageDialog(null, "Your guess is out of range. Try again.",
-	                            "Invalid Guess", JOptionPane.WARNING_MESSAGE);
-	                } else {
-	                    JOptionPane.showMessageDialog(null, determineGuess(userAns, compNumber, count),
-	                            "Result", JOptionPane.INFORMATION_MESSAGE);
-	                    count++;
-	                }
-	            } catch (NumberFormatException e) {
-	                JOptionPane.showMessageDialog(null, "Invalid input. Please enter a number.",
-	                        "Invalid Input", JOptionPane.ERROR_MESSAGE);
-	            }
-	        }
-	        int playAgain = JOptionPane.showConfirmDialog(null, "Congratulations! You guessed the number in " + (count - 1) + " attempts.\nDo you want to play again?",
-	                "Game Over", JOptionPane.YES_NO_OPTION);
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("Try to guess the number between " + lowerBound + " and " + upperBound);
 
-	        if (playAgain == JOptionPane.YES_OPTION) {
-	            playGame();
-	        } else {
-	            JOptionPane.showMessageDialog(null, "Thank you for playing. Goodbye!", "Game Over", JOptionPane.INFORMATION_MESSAGE);
-	        }
-	    }
-	    public static String determineGuess(int userAns, int compNumber, int count) {
-	        if (userAns == compNumber) {
-	            return "Correct!\nTotal Guesses: " + count;
-	        } else if (userAns > compNumber) {
-	            return "Your guess is too high. Try again.\nTry Number: " + count;
-	        } else {
-	            return "Your guess is too low. Try again.\nTry Number: " + count;
-	        }
-	    }
-	}
+        for (int attempts = 1; attempts <= maxAttempts; attempts++) {
+            System.out.print("Attempt " + attempts + ": Enter your guess: ");
+            int userGuess = scanner.nextInt();
+
+            if (userGuess == randomNumber) {
+                gameWon = true;
+                score = maxAttempts - attempts + 1;
+                break;
+            } 
+	    else if (userGuess < randomNumber) {
+                System.out.println("Too low! Try again.");
+            } 
+	    else {
+		    System.out.println("Too high! Try again.");
+            }
+        }
+
+        if (gameWon) {
+            System.out.println("Congratulations! You guessed the number. Your score: " + score);
+        } else {
+            System.out.println("Sorry, you ran out of attempts. The correct number was: " + randomNumber);
+        }
+    }
+}
